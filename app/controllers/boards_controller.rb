1f7ad28343a8526aeb14110f109fd76184020f9f
Taylor Collins <taylorcc33@gmail.com>
2020-11-30 21:55:25 -0700

    updated ui, user boards index

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,13 +1,28 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @boards = Board.all
   end
 
   def show
   end
 
   def new
+    @board = Board.new
+  end
+  
+  def create
   end
 
   def edit
   end
+
+  def update
+  end
+
+  private
+    def set_board
+      @board = Board.find(params[:id])
+    end
 end
