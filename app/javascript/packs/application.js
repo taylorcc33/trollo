3ad26fe645f699a0c19cd1dbd888248acf97aad6
Taylor Collins <taylorcc33@gmail.com>
2020-12-04 23:17:50 -0700

    added navbar, mobile needs work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
-require("semantic-ui-sass")
+require("@rails/ujs").start();
+require("@rails/activestorage").start();
+require("channels");
+require("semantic-ui-sass");
 
+// Loads all Semantic javascripts
+//= require semantic-ui
+
+//= require semantic-ui/modal
+//= require semantic-ui/dropdown
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
